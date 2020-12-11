ab9b7bcdf4abf59bec8f4b7ea759ca84b6c0e5df
Gibson <ypsvlq@gmail.com>
2020-12-11 17:30:31 +0000

    List bucket

--- src/common.nim
+++ src/common.nim
@@ -1,5 +1,22 @@
-proc install*(die: proc(s: string), mainstatus: proc(s: string), status: proc(s: string)) =
+import httpclient, xmltree, xmlparser
+
+var
+  die: proc(s: string)
+  status: proc(s: string)
+
+proc download(url: string) =
+  var client = newHttpClient()
+
+  status("Loading file list...")
+  var bucket = parseXml(client.get(url).bodyStream)
+
+  for node in bucket:
+    if tag(node) == "Contents":
+      status(node.child("Key").innerText)
+
+proc install*(pDie: proc(s: string), mainstatus: proc(s: string), pStatus: proc(s: string)) =
+  die = pDie
+  status = pStatus
+
   mainstatus("Installing")
-  status("Test")
-  status("Test 2")
-  die("Failed")
+  download("https://quetoo.s3.amazonaws.com/")
