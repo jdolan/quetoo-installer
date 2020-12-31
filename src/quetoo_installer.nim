7e261e10379e8068cd7eb8f01464cd8d5f7172d7
Gibson <ypsvlq@gmail.com>
2020-12-31 16:43:18 +0000

    Restructure quetoo_installer.nim

--- src/quetoo_installer.nim
+++ src/quetoo_installer.nim
@@ -1,12 +1,23 @@
 import common, nigui, nigui/msgbox
 
+var opts = newInstallerOptions()
+
+app.init()
+
+var win = newWindow("Quetoo Installer")
+win.width = 400.scaleToDpi
+win.height = 125.scaleToDpi
+
+var container = newLayoutContainer(Layout_Vertical)
+container.padding = 10
+win.add(container)
+
+var button = newButton(if isNewInstall(opts): "Install" else: "Update")
+container.add(button)
+
 var
-  win: Window
-  box: LayoutContainer
-  button: Button
   label1, label2: Label
   pbar: ProgressBar
-  thread: Thread[void]
 
 proc die(s: string) =
   {.gcsafe.}:
@@ -28,38 +39,25 @@ proc status(s: string, progress: float) =
       pbar.value = progress
     )
 
-var opts = newInstallerOptions()
-
-app.init()
-
-win = newWindow("Quetoo Installer")
-win.width = 400.scaleToDpi
-win.height = 125.scaleToDpi
-
-box = newLayoutContainer(Layout_Vertical)
-box.padding = 10
-win.add(box)
-
-button = newButton(if isNewInstall(opts): "Install" else: "Update")
-box.add(button)
-
 proc start() =
   {.gcsafe.}:
     install(opts, die, mainstatus, status)
     while app.queued() > 0:
       discard
 
+var thread: Thread[void]
+
 button.onClick = proc(event: ClickEvent) =
-  box.remove(button)
+  container.remove(button)
 
   label1 = newLabel("Initialising")
-  box.add(label1)
+  container.add(label1)
 
   pbar = newProgressBar()
-  box.add(pbar)
+  container.add(pbar)
 
   label2 = newLabel("")
-  box.add(label2)
+  container.add(label2)
 
   createThread(thread, start)
 
