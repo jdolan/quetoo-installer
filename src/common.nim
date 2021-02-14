cd4e209961182ecc9ff97cadb0654945eef7ecb1
Gibson <ypsvlq@gmail.com>
2021-02-14 14:15:29 +0000

    Support options everywhere

--- src/common.nim
+++ src/common.nim
@@ -1,4 +1,4 @@
-import httpclient, xmltree, xmlparser, sugar, strutils, os, md5, uri
+import httpclient, xmltree, xmlparser, sugar, strutils, os, md5, uri, parseopt
 
 when defined(windows):
   when defined(i386):
@@ -88,9 +88,49 @@ proc isNewInstall*(opts: InstallerOptions): bool =
       return true
   return not dirExists(opts.dir & "/" & check)
 
-proc install*(opts: InstallerOptions, pDie: proc(s: string), mainstatus: proc(s: string), pStatus: proc(s: string, progress: float)) =
+let help = """
+Quetoo Installer
+
+Usage: """ & paramStr(0) & """ [options] [<dir>]
+
+Options:
+  -h      --help       Show this message.
+  -b      --bin        Only update binaries.
+  -d      --data       Only update data.
+  -k      --keep       Don't delete unrecognised/outdated files.
+  -o<os>  --os <os>    Override OS detection (windows, mingw, linux, macosx)
+  -c<cpu> --cpu <cpu>  Override OS detection (i386, amd64)
+"""
+
+proc install*(opts: var InstallerOptions, pDie: proc(s: string), mainstatus: proc(s: string), pStatus: proc(s: string, progress: float)) =
   die = pDie
   status = pStatus
+
+  for kind, key, val in getopt(commandLineParams(), {'h', 'b', 'd', 'k'}, @["help", "bin", "data", "keep"]):
+    case kind:
+      of cmdArgument:
+        opts.dir = key
+      of cmdLongOption, cmdShortOption:
+        case key:
+          of "help", "h", "?":
+            echo(help)
+            quit(0)
+          of "bin", "b":
+            opts.installData = false
+          of "data", "d":
+            opts.installBin = false
+          of "keep", "k":
+            opts.purge = false
+          of "os", "o":
+            opts.os = val
+          of "cpu", "c":
+            opts.cpu = val
+          of "build":
+            opts.forceTriple = val
+          else:
+            echo "warning: unrecognised argument " & key
+      of cmdEnd:
+        assert(false)
 
   var triple = opts.forceTriple
   if triple == "":
--- src/quetoo_installer_cli.nim
+++ src/quetoo_installer_cli.nim
@@ -1,4 +1,4 @@
-import common, os, parseopt
+import common
 
 proc die(s: string) =
   echo("\n", s)
@@ -10,46 +10,5 @@ proc mainstatus(s: string) =
 proc status(s: string, progress: float) =
   echo(s)
 
-let help = """
-Quetoo Installer
-
-Usage: """ & paramStr(0) & """ [options] [<dir>]
-
-Options:
-  -h      --help       Show this message.
-  -b      --bin        Only update binaries.
-  -d      --data       Only update data.
-  -k      --keep       Don't delete unrecognised/outdated files.
-  -o<os>  --os <os>    Override OS detection (windows, mingw, linux, macosx)
-  -c<cpu> --cpu <cpu>  Override OS detection (i386, amd64)
-"""
-
 var opts = newInstallerOptions()
-
-for kind, key, val in getopt(commandLineParams(), {'h', 'b', 'd', 'k'}, @["help", "bin", "data", "keep"]):
-  case kind:
-    of cmdArgument:
-      opts.dir = key
-    of cmdLongOption, cmdShortOption:
-      case key:
-        of "help", "h", "?":
-          echo(help)
-          quit(0)
-        of "bin", "b":
-          opts.installData = false
-        of "data", "d":
-          opts.installBin = false
-        of "keep", "k":
-          opts.purge = false
-        of "os", "o":
-          opts.os = val
-        of "cpu", "c":
-          opts.cpu = val
-        of "build":
-          opts.forceTriple = val
-        else:
-          echo "warning: unrecognised argument " & key
-    of cmdEnd:
-      assert(false)
-
 install(opts, die, mainstatus, status)
